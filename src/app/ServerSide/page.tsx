4c3b5dd9bd277818a880adc0a744796218bb8d0a
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-02-08 01:43:53 +0500

    datafetchingCss

---
+++ src/app/ServerSide/page.tsx
@@ -0,0 +1,46 @@
+import styles from "./sever.module.css"
+async function ServerSidePage() {
+  interface Book {
+    id: number;
+    name: string;
+    type: string;
+    available: boolean;
+  }
+
+  const res = await fetch("https://simple-books-api.glitch.me/books");
+  const resp: Book[] = await res.json();
+  // console.log("Books fetched:", resp);
+
+  return (
+    <div  style={{ display: "flex", color:"black", padding: "16px", justifyContent:"center", alignItems:"center",height:"100vh", backgroundColor:"#f3f4f6" }} >
+      {resp.length > 0 ? (
+        <div className={styles.gridContainer}>
+          {resp.map((book) => (
+            <div
+              key={book.id}
+              style={{backgroundColor:"white",borderRadius:"0.5rem",border:"#e5e7eb", padding:"24px" }}>
+              <ul style={{display:"flex", flexDirection:"column",gap:"8px", listStyle:"none"}}>
+                <li style={{fontSize:"18px",fontWeight:"600px"}}>Name: {book.name}</li>
+                <li>Type: {book.type}</li>
+                <li>
+                  Available:{" "}
+                  <span
+                    className={
+                      book.available ? "text-green" : "text-red"
+                    }
+                  >{`${book.available}`}</span>
+                </li>
+              </ul>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div style={{fontSize:"18px",color:"#4b5563"}}>
+          Loading or no books available...
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ServerSidePage;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
-import styles from "./page.module.css";
 import Link from "next/link";
-
-export default function Home() {
+import styles from "./Navbar.module.css"
+const Navbar = () => {
   return (
-    <div className={styles.div} style={{ height: "100vh", backgroundColor: "#cbd5e1" }}>
-      <img src="/7.png" alt="Logo " />
+    <nav  style={{ backgroundColor: "#3b82f6", padding: "16px" , color:"white"}}>
+      <div  style={{ display: "flex", justifyContent:"space-between" , fontWeight: "600px", listStyle: "none" ,alignItems:"center"}}  className={styles.flex}>
+        <Link href="/">
+        <img src="/7.png" alt="Logo " />
+        </Link>
       <ul className=" gap-4 text-2xl font-semibold" style={{ display: "flex", gap: "16px", fontSize: "24px", fontWeight: "600px", listStyle: "none" }} >
         <li>
           <Link
@@ -21,6 +23,9 @@ export default function Home() {
           </Link>{" "}
         </li>
       </ul>
-    </div>
+      </div>
+    </nav>
   );
-}
+};
+
+export default Navbar;
